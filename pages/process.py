d49d6ce5cc024b204fb63f208dc429950065c2ee
Taylor <bickell.taylor@gmail.com>
2020-07-08 15:18:18 -0700

    Update insights and process pages.

--- pages/process.py
+++ pages/process.py
@@ -10,18 +10,49 @@ from app import app
 
 # 1 column layout
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
-column1 = dbc.Col(
-    [
-        dcc.Markdown(
-            """
-        
-            ## Process
+# column1 = dbc.Col(
+#     [dcc.Markdown(
+#             """
+#             ### Process
+
+#             """
+#         ),
+
+#     ],
+# )
 
 
-            """
-        ),
 
-    ],
-)
+row1 = html.Div([
+    html.H4("Defining the Machine Learning Problem"),
+    html.Br(),
+    html.Div([
+        html.H5("What feature in the dataset was used as the target?", style={'font-style':'italic'}),
+        html.P("Price was used as the target variable."),
+        html.H5("Is this a regression or classification problem?", style={'font-style':'italic'}),
+        html.P("This is a regression problem given that price, a continuous  is being predicted."),
+        html.H5("How is the target distributed?", style={'font-style':'italic'}),
+        # html.Img(src="assets/ebay_target_distribution.PNG", className="img-fluid"),
+        html.H5("What metric is used to evaluate the performance of the model?", style={'font-style':'italic'}),
+        html.P("Mean Absolute Error is used to evaluate the model. MAE explains a model's performance in terms of answering how far off a prediction is on average."),
+        html.H5("What is the size of the train and test set?", style={'font-style':'italic'}),
+        html.P("Training data shape: (74, 5)"),
+        html.P("Testing data shape: (25, 5)")
+    ])
+])
 
-layout = dbc.Row([column1])
+row2 = html.Div([
+    html.Br(),
+    html.Div([
+        html.H4("Exploring the Data Cleaning Process"),
+        html.P("Details will go here."),
+        html.Br(),
+        html.H4("Explaining Model Selection and Model Performance"),
+        html.P("Details will go here.")
+    ])
+])
+
+
+layout = dbc.Row([row1, row2])
+
+# layout = dbc.Row([column1])
